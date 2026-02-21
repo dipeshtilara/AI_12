a49655441525ea0836c3aa4b59f0ad5c41b2d5f2
DPST <62595334+dipeshtilara@users.noreply.github.com>
2026-02-21 14:01:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -18,13 +18,30 @@ def main():
     html_file_path = "index.html"
     
     if os.path.exists(html_file_path):
-        # We use an iframe to render the static site. 
-        # Note: Depending on Streamlit's environment, local CSS and JS files linked in the HTML 
-        # might need to be in a static folder or hosted if they don't load correctly in the iframe.
-        # Streamlit 1.28+ supports a `static` folder out of the box.
+        import re
         with open(html_file_path, 'r', encoding='utf-8') as f:
             html_content = f.read()
             
+        def get_file_content(path):
+            if os.path.exists(path):
+                with open(path, 'r', encoding='utf-8') as f:
+                    return f.read()
+            return ""
+
+        # Inline CSS
+        css_content = get_file_content("css/style.css")
+        html_content = re.sub(r'<link\s+rel="stylesheet"\s+href="css/style\.css">', f'<style>{css_content}</style>', html_content)
+        
+        # Inline JS files
+        curr_js = get_file_content("js/data/curriculum.js")
+        html_content = re.sub(r'<script\s+src="js/data/curriculum\.js"></script>', f'<script>{curr_js}</script>', html_content)
+        
+        vis_js = get_file_content("js/components/visualizer.js")
+        html_content = re.sub(r'<script\s+src="js/components/visualizer\.js"></script>', f'<script>{vis_js}</script>', html_content)
+        
+        main_js = get_file_content("js/main.js")
+        html_content = re.sub(r'<script\s+src="js/main\.js"></script>', f'<script>{main_js}</script>', html_content)
+
         st.components.v1.html(html_content, height=800, scrolling=True)
     else:
         st.error(f"Could not find the file: {html_file_path}. Please make sure you are running the app from the correct directory.")
